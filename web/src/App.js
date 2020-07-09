b9e4d1b10a35e83fe7a7b86f6a5601cad8cb58f5
Dakota Brink <779390+codabrink@users.noreply.github.com>
2020-07-08 23:10:48 -0400

    handle resizing

--- web/src/App.js
+++ web/src/App.js
@@ -1,30 +1,35 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useLayoutEffect } from 'react'
 import * as chart from './lib/chart'
 import { Select } from './UI'
 
 function App() {
-  let [frames, setFrames] = useState([])
-  let [interval, setInterval] = useState("15m")
-  let [symbol, setSymbol] = useState("BTCUSDT")
+  let [data, setData] = useState()
+  let [interval, setInterval] = useState('15m')
+  let [symbol, setSymbol] = useState('BTCUSDT')
 
   useEffect(() => {
     fetch(`/prices?symbol=${symbol}&interval=${interval}`)
       .then((r) => r.json())
-      .then((frames) => {
-        setFrames(frames)
-        chart.drawChart(frames)
+      .then((data) => {
+        setData(data)
+        chart.drawChart(data)
       })
   }, [symbol, interval])
+
+  useEffect(() => {
+    function resized(e) {
+      if (!data) return
+      chart.drawChart(data)
+    }
+
+    window.addEventListener('resize', resized)
+    return () => window.removeEventListener('resize', resized)
+  }, [data])
 
   return (
     <>
       <div id="control-bar" className="p-3 flex">
-        <Select
-          label="Symbol"
-          options={['BTCUSDT']}
-          value={symbol}
-          onChange={setSymbol}
-        />
+        <Select label="Symbol" options={['BTCUSDT']} value={symbol} onChange={setSymbol} />
         <Select
           label="Interval"
           options={['5m', '15m', '30m', '1h', '4h', '1d']}
@@ -33,7 +38,7 @@ function App() {
         />
       </div>
       <div id="chart-container" className="flex-grow w-full">
-        <svg id="chart" key={interval + symbol} />
+        <svg id="chart" />
       </div>
     </>
   )
